7bbc7078131eefee3c198c2465c3b8b1c8f28761
Julian Vega <ingjulianvega@gmail.com>
2021-07-25 09:52:04 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasumedicine/web/model/Medicine.java
+++ src/main/java/ingjulianvega/ximic/msscasumedicine/web/model/Medicine.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
@@ -15,6 +16,7 @@ public class Medicine implements Serializable {
 
     static final long serialVersionUID = 5697732515185505131L;
 
+    @NotBlank
     private String name;
 
 }
